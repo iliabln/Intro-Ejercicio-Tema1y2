e6c3a201e7395ee912cb9ebd9820f4b5cb0180a0
iliabln <iliablohin@gmail.com>
2022-03-02 19:25:59 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int numero1 = 0;
+
+        System.out.println(numero1);
+
+        numero1 = 5;
+
+        System.out.println(numero1);
+
+        String[] ciudades = new String []{"Madrid", "Barcelona", "Granada", "Sevilla"};
+
+        System.out.println(ciudades[0]);
+
+    }
+    
+}
